f24169fd158b71bc1cc3079b24f6208919e84d88
Gabriel <gabrielhenriquedps@gmail.com>
2024-03-22 21:09:01 -0300

    feat: add script

--- script.js
+++ script.js
@@ -2,8 +2,22 @@ const validate = () => {
     let forms = document.querySelectorAll('.needs-validation');
     forms.forEach((form) => {
         form.addEventListener('submit', (event) => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
+            event.preventDefault();
+            if (form.checkValidity()) {
+                const payload = {
+                    name: form.querySelector('#nome').value,
+                    surname: form.querySelector('#surname').value,
+                    birthDate: form.querySelector('#birthday').value,
+                    profession: form.querySelector('#profission').value,
+                    documentNumber: form.querySelector('#cpf').value,
+                    email: form.querySelector('#email').value,
+                    password: form.querySelector('#pwd').value,
+                    phone: form.querySelector('#telefone').value,
+                    address: form.querySelector('#address').value,
+                };
+                alert(`Cadastro realizado com sucesso! \n ${JSON.stringify(payload)}`);
+                console.log(payload);
+            } else {
                 event.stopPropagation();
             }
             form.classList.add('was-validated');
